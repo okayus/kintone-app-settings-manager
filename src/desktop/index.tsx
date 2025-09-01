7a64196fad0a6e10286664e4ce54166c12cb6d0b
okayus <toshiaki.mukai.9981@gmail.com>
2025-09-01 17:46:26 +0900

    change:desktop初期化

--- src/desktop/index.tsx
+++ src/desktop/index.tsx
@@ -1,11 +1,6 @@
 import { KintoneRestAPIClient } from "@kintone/rest-api-client";
 
-import { KintoneSdk } from "../shared/util/kintoneSdk";
-
 import { renderExecutionButton } from "./components/desktopUIHelpers";
-import { MessageService } from "./service/MessageService";
-
-import type { ConfigSchema } from "../shared/types/Config";
 
 // メイン処理
 ((PLUGIN_ID) => {
@@ -13,17 +8,11 @@ import type { ConfigSchema } from "../shared/types/Config";
     const pluginConfig = kintone.plugin.app.getConfig(PLUGIN_ID).config;
     if (!pluginConfig) return;
 
-    const config: ConfigSchema = JSON.parse(pluginConfig).config;
     const restApiClient = new KintoneRestAPIClient();
-    const kintoneSdk = new KintoneSdk(restApiClient);
-    const messageService = new MessageService(config, kintoneSdk);
 
     const handleAlertButtonClick = async () => {
       try {
         // 設定されたアプリからレコードを取得
-        const recordsWithSettings =
-          await messageService.fetchRecordsFromSettings();
-        messageService.alertMessage(recordsWithSettings);
       } catch (error) {
         console.error("Error:", error);
         alert("エラーが発生しました: " + error);
